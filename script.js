e3a1cbdc26393615e511385d6f870519f02389da
Marwen Krir <marwen.kr7@gmail.com>
2024-12-03 18:25:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.fa-bars');
+let navMobile = document.querySelector('.mobile-nav');
+
+burger.addEventListener('click', function(){
+navMobile.classList.toggle('classlistBurger');
+});
